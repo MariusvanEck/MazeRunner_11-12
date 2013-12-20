4bfe87d457e56ac9c4b268aead1cae7301e06732
MariusvanEck <m.t.vaneck@student.tudelft.nl>
2013-12-20 15:39:03 +0100

    Sound loop en stop toegevoegd

--- MazeRunner/src/gamestate/Sound.java
+++ MazeRunner/src/gamestate/Sound.java
@@ -7,28 +7,56 @@ import java.net.URL;
 public class Sound {
 
    private AudioClip clip;
+   private Thread soundThread;
    
-   public Sound(String name){
-      try
-      {
-    	 URL url = new URL("file:sound/" + name);
-         clip = Applet.newAudioClip(url);
-      }catch (Throwable e){
-         e.printStackTrace();
-      }
-   }
+   /** 
+    * create a new sound
+    */
+   	public Sound(String name) {
+   		try {
+   			URL url = new URL("file:sound/" + name);
+   			clip = Applet.newAudioClip(url);} 
+   		catch (Throwable e){e.printStackTrace();}
+   	}
    
-   public void play(){
-      try{
-         new Thread(){
-            public void run(){
-            	clip.play();
-            }
-         }.start();
-      }catch(Throwable e){
-         e.printStackTrace();
-      }
+   /** 
+    * play the sound
+    */
+   	public void play() {
+   		try {
+   			soundThread = 	new Thread(){
+        	 				public void run(){clip.play();}
+           					};
+         
+           	soundThread.start();}
+   		catch(Throwable e){e.printStackTrace();}
+   	}
+   
+   /**
+    * loop the sound
+    */
+   	public void loop() {
+	      try{
+	         soundThread = 	new Thread(){
+	        	 			public void run(){clip.loop();}
+	         				};
+	         				
+	         soundThread.start();}
+	      catch(Throwable e){e.printStackTrace();}
    }
+   	
+   	/**
+   	 * stop the sound;
+   	 */
+   	public void stop() {
+	      try{
+	         soundThread = 	new Thread(){
+	        	 			public void run(){clip.stop();}
+	         				};
+	         				
+	         soundThread.start();}
+	      catch(Throwable e){e.printStackTrace();}
+ }
 }
 
 
