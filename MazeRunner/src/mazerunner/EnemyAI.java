6b2b82eb6c8f338d239137b39e8d390f65d35fad
TeleKD <k.vandonge@student.tudelft.nl>
2014-01-07 12:24:41 +0100

    Textures op OBJ werken!

--- MazeRunner/src/mazerunner/EnemyAI.java
+++ MazeRunner/src/mazerunner/EnemyAI.java
@@ -47,7 +47,7 @@ public class EnemyAI{
 			// add an enemy
 			enemies.add(new Enemy(	gl, x, 0, z, 
 									rnd.nextDouble()*360 - 180,  
-									new Stick(gl), "models/test.obj"));}
+									new Stick(gl),  "models/Lambent_Male/Lambent_Male.obj"));}
 	}
 	
 	
